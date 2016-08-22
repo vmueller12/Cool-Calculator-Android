b18d95b2f7401722eedba519f03ccf5f7566b242
Vitali Müller <Vitali Mller>
2016-08-22 21:46:52 +0100

    created in MainActivity variable names and created onClickListener for the Btn

--- app/src/main/java/com/vitalimueller/coolcalculator/MainActivity.java
+++ app/src/main/java/com/vitalimueller/coolcalculator/MainActivity.java
@@ -2,6 +2,10 @@ package com.vitalimueller.coolcalculator;
 // change the style of the Activity
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +13,139 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button oneBtn = (Button)findViewById(R.id.onebtn);
+        Button twoBtn = (Button)findViewById(R.id.twoBtn);
+        Button threebtn = (Button)findViewById(R.id.threeBtn);
+        Button fourBtn = (Button)findViewById(R.id.fourBtn);
+        Button fiveBtn = (Button)findViewById(R.id.fiveBtn);
+        Button sixBtn = (Button)findViewById(R.id.sixBtn);
+        Button sevenBtn = (Button)findViewById(R.id.sevenBtn);
+        Button eightBtn = (Button)findViewById(R.id.eightBtn);
+        Button nineBtn = (Button)findViewById(R.id.nineBtn);
+        Button zeroBtn = (Button)findViewById(R.id.zeroBtn);
+
+        Button clearBtn = (Button)findViewById(R.id.clearBtn);
+        ImageButton divideBtn = (ImageButton)findViewById(R.id.divBtn);
+        ImageButton multiplyBtn = (ImageButton)findViewById(R.id.multBtn);
+        ImageButton additionBtn = (ImageButton)findViewById(R.id.addBtn);
+        ImageButton subtractionBtn = (ImageButton)findViewById(R.id.subtBtn);
+        ImageButton calcBtn = (ImageButton)findViewById(R.id.calcBtn);
+
+        TextView solutionView = (TextView)findViewById(R.id.solutionView);
+
+        oneBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        twoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        threebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        fourBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        fiveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        sixBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        sevenBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        eightBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        nineBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        zeroBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        additionBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        subtractionBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        multiplyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        divideBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        clearBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        calcBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
+
+
     }
 }
